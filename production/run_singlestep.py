c1d28309b84d11fe3f18318e1a0709feded0e30f
Erica Brondolin <erica.brondolin@cern.ch>
2021-03-17 11:57:20 +0100

    Clean up and factorise run_singlestep

--- production/run_singlestep.py
+++ production/run_singlestep.py
@@ -15,6 +15,7 @@ VERBOSE = args.verbose
 if VERBOSE : print(args)
 
 TEST = args.test
+TAG  = args.tag
 
 samples = args.sample
 if args.sample == ['all']:
@@ -25,45 +26,48 @@ if args.energies == ['all']:
   energies = ['10', '20', '50', '100', '200', '300']
 
 genProducer = {'singlephoton':"closeBy", 'singlepi':"flatEGun", 'singleel':"flatEGun", 'singleKaonL':"closeBy"}
-folder = '/data2/user/ebrondol/HGCal/production/'+args.tag+'/CMSSW_11_3_0_pre4/'
+folder = '/data2/user/ebrondol/HGCal/production/'+TAG+'/CMSSW_11_3_0_pre4/'
 
 if args.step == 'step1':
   if VERBOSE : print('running step1')
   for sample in samples:
     outputFolder = "{}/{}_{}_hgcalCenter/".format(folder, sample, genProducer[sample])
     for en in energies:
-      command = "cmsRun step1_%s.py %s %s %s >& log_%s_%s_%sGeV.log &"%(genProducer[sample], en, sample, outputFolder, args.step, sample, en)
+      log = 'log/'+TAG+'_'+args.step+'_'+sample+'_'+en+'GeV.log'
+      command = "cmsRun step1_%s.py %s %s %s >& %s &"%(genProducer[sample], en, sample, outputFolder, log)
       print(command)
       if not TEST:
         os.system(command)
-
-if args.step == 'step2':
-  if VERBOSE : print('running step2')
+else:
+  if VERBOSE : print('running %s'%args.step)
   for sample in samples:
     outputFolder = "{}/{}_{}_hgcalCenter/".format(folder, sample, genProducer[sample])
     for en in energies:
-      command = "cmsRun step2.py %s %s %s >& log_%s_%s_%sGeV.log &"%(en, sample, outputFolder, args.step, sample, en)
+      log = 'log/'+TAG+'_'+args.step+'_'+sample+'_'+en+'GeV.log'
+      command = "cmsRun %s.py %s %s %s >& %s &"%(args.step, en, sample, outputFolder, log)
       print(command)
       if not TEST:
         os.system(command)
 
-if args.step == 'step3':
-  if VERBOSE : print('running step3')
-  for sample in samples:
-    outputFolder = "{}/{}_{}_hgcalCenter/".format(folder, sample, genProducer[sample])
-    for en in energies:
-      command = "cmsRun step3.py %s %s %s >& log_%s_%s_%sGeV.log &"%(en, sample, outputFolder, args.step, sample, en)
-      print(command)
-      if not TEST:
-        os.system(command)
-
-if args.step == 'step4':
-  if VERBOSE : print('running step4')
-  for sample in samples:
-    outputFolder = "{}/{}_{}_hgcalCenter/".format(folder, sample, genProducer[sample])
-    for en in energies:
-      command = "cmsRun step4.py %s %s %s >& log_%s_%s_%sGeV.log &"%(en, sample, outputFolder, args.step, sample, en)
-      print(command)
-      if not TEST:
-        os.system(command)
-
+#if args.step == 'step3':
+#  if VERBOSE : print('running step3')
+#  for sample in samples:
+#    outputFolder = "{}/{}_{}_hgcalCenter/".format(folder, sample, genProducer[sample])
+#    for en in energies:
+#      log = 'log/'+TAG+'_'+args.step+'_'+sample+'_'+en+'GeV.log'
+#      command = "cmsRun step3.py %s %s %s >& %s &"%(en, sample, outputFolder, log)
+#      print(command)
+#      if not TEST:
+#        os.system(command)
+#
+#if args.step == 'step4':
+#  if VERBOSE : print('running step4')
+#  for sample in samples:
+#    outputFolder = "{}/{}_{}_hgcalCenter/".format(folder, sample, genProducer[sample])
+#    for en in energies:
+#      log = 'log/'+TAG+'_'+args.step+'_'+sample+'_'+en+'GeV.log'
+#      command = "cmsRun step4.py %s %s %s >& %s &"%(en, sample, outputFolder, log)
+#      print(command)
+#      if not TEST:
+#        os.system(command)
+#
