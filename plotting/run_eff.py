6d62ea51074afa2f7865bade5808e256ffe13890
Leonardo Cristella <leonardo.cristella@cern.ch>
2021-04-06 21:32:26 +0200

    Add line before Merge bin

--- plotting/run_eff.py
+++ plotting/run_eff.py
@@ -7,7 +7,7 @@ SAMPLES = ["singlephoton", "singlepi", "singleel", "singleKaonL"]
 #SAMPLES = ["singlepi"]
 GENPRODUCER = {'singlephoton':"closeBy", 'singlepi':"flatEGun", 'singleel':"flatEGun", 'singleKaonL':"closeBy"}
 
-RELEASE_TAG = ["vanilla"]
+RELEASE_TAG = ["tLine"]
 
 iterLabel = OrderedDict([#("Dummy","Dummy"), 
 			 ("TrkEM","TrkEM"), ("EM","EM"), ("Trk","TrkHAD"), ("HAD","HAD")
@@ -16,7 +16,7 @@ iterLabel = OrderedDict([#("Dummy","Dummy"),
 ITERS = [' '.join("\""+iterLabel[ticlIter]+":ticlMultiClustersFromTracksters"+ticlIter+"\"" for ticlIter in iterLabel)]
 
 #PATHFILESIN = "/data2/user/ebrondol/HGCal/HGCDoublet_validation/CMSSW_11_2_0_pre10/TICLv3_11_2_X_pre10/production/"
-PATHFILESIN = "/data2/user/lecriste/HGCal/samples/TICLDebugger/EMTrackSeeded/92c59aa-pre10_model138329a/"
+PATHFILESIN = "/data2/user/lecriste/HGCal/samples/TICLDebugger/EMTrackSeeded/11_3_0_pre2_modelmemoryFix/"
 
 energies_features = OrderedDict([#("10",91), 
 ("50",94), ("100",64), ("200",57), ("300",52)])
@@ -36,7 +36,7 @@ for tag,i_iter in zip(RELEASE_TAG,ITERS) :
       os.makedirs(PATHFILESOUT)
 
 
-    command_plot = "python produce_plots_plotting_eff.py --filesin %s --folderout %s --sample %s --features %s --iter %s -v"%(listFilein,PATHFILESOUT,i_label,FEATURES,i_iter)
+    command_plot = "python produce_plots_plotting_eff.py --filesin %s --folderout %s --sample %s --features %s --iter %s -v"%(listFilein, PATHFILESOUT, i_label,FEATURES, i_iter)
 
     print(command_plot)
     os.system(command_plot)
